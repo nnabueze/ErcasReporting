cfa2de0ca91656d8ca5d6fbd39bc566b86d69ef2
Nnabueze Opara <villavelle101@gmail.com>
2021-05-29 03:16:20 +0100

    abt installing async storage

---
+++ src/screens/splash/Splash.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  Button,
+  Left,
+  Right,
+  Body,
+  Icon,
+  Text,
+  View,
+  Picker,
+  Form,
+} from "native-base";
+
+const Splash = () => {
+  return (
+    <View>
+      <StatusBar hidden={true} />
+      <Text>Splash Screen</Text>
+    </View>
+  );
+};
+
+export default Splash;
